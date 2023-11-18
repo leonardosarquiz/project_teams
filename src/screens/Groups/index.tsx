02ad9f0b3c583bf5a75dfd7a023c1b3d1bf5afa0
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-18 14:16:23 -0300

    add ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { Header } from '@components/Header';
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
-  const [groups, setGroups] = useState<string[]>(['Galera do fut']);
+  const [groups, setGroups] = useState<string[]>([]);
   return (
     <Container>
       <Header />
@@ -16,6 +17,8 @@ export function Groups() {
         data={groups}
         keyExtractor={item => item}
         renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
       />
 
     </Container>
