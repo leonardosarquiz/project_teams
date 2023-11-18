de106477e4ebb17de39f42bbeeaddaf8ec18ec78
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-18 16:44:53 -0300

    add component button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,7 +4,8 @@ import { Header } from '@components/Header';
 import { Container } from './styles'
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
-import { ListEmpty } from '@components/ListEmpty'
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -19,6 +20,10 @@ export function Groups() {
         renderItem={({ item }) => <GroupCard title={item} />}
         contentContainerStyle={groups.length === 0 && { flex: 1 }}
         ListEmptyComponent={() => <ListEmpty message='Que tal cadastrar a primeira turma?' />}
+      />
+
+      <Button
+        title='Criar nova turma '
       />
 
     </Container>
