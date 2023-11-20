2eb0037101220d6b73d58bc06f8d7339dae31007
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-20 16:03:25 -0300

    add component buttonIcon

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,6 +1,8 @@
 import { Header } from "@components/Header";
-import { Container } from "./styles";
+import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
+import { ButtonIcon } from "@components/ButtonIcon";
+import { Input } from "@components/Input";
 
 
 export function Players() {
@@ -10,6 +12,12 @@ export function Players() {
 
       <Highlight title="Nome da turma" subtitle="adicione a galera e separe os times" />
 
+
+      <Form>
+        <Input placeholder="Nome da pessoa" autoCorrect={false} />
+
+        <ButtonIcon icon="add" />
+      </Form>
     </Container>
   )
 }
