70f27f2a4999650937f05cda6270c868996b38e7
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-20 16:41:23 -0300

    add component filter

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -3,6 +3,7 @@ import { Container, Form } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
+import { Filter } from "@components/Filter";
 
 
 export function Players() {
@@ -18,6 +19,8 @@ export function Players() {
 
         <ButtonIcon icon="add" />
       </Form>
+
+      <Filter title="time A" />
     </Container>
   )
 }
