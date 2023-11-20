72ed35a7ace219ba731824890caa0dec05fc1dd9
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-20 17:16:48 -0300

    refactored players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,12 +1,16 @@
 import { Header } from "@components/Header";
-import { Container, Form } from "./styles";
+import { Container, Form, HeaderList, NumbersOfPlayers } from "./styles";
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
+import { FlatList } from "react-native";
+import { useState } from "react";
 
 
 export function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -20,7 +24,21 @@ export function Players() {
         <ButtonIcon icon="add" />
       </Form>
 
-      <Filter title="time A" />
+      <HeaderList>
+
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter title={item} isActive={item === team} onPress={() => setTeam(item)} />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
+
     </Container>
   )
 }
