7c4909c44941bd78e4de059e3692718b62d092c3
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-20 10:39:33 -0300

    add component newGroup

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components'
 import { Loading } from '@components/Loading'
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import theme from './src/theme'
-import { Groups } from '@screens/Groups';
+import { NewGroup } from '@screens/NewGroup';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -14,7 +14,7 @@ export default function App() {
         backgroundColor='transparent'
         translucent
       />
-      {fontsLoaded ? <Groups /> : < Loading />}
+      {fontsLoaded ? <NewGroup /> : < Loading />}
     </ThemeProvider>
   );
 }
---
+++ src/screens/NewGroup/styles.ts
@@ -0,0 +1,19 @@
+import styled from 'styled-components/native';
+import { FontAwesome } from '@expo/vector-icons';
+
+
+export const Container = styled.View`
+flex: 1;
+background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+padding: 24px;
+`;
+
+export const Content = styled.View`
+flex: 1;
+justify-content: center;
+
+`;
+
+export const Icon = styled(FontAwesome).attrs(({ theme }) => ({ color: theme.COLORS.GREEN_700 }))`
+align-self: center;
+`;
