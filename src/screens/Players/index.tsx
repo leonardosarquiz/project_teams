c043ae161ff26d44c97d7d22bba7a7acb94a7730
Leonardo Sarquiz Hugenthobler <leonardosarquiz14@gmail.com>
2023-11-20 17:51:30 -0300

    add button in players

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -7,11 +7,13 @@ import { Filter } from "@components/Filter";
 import { FlatList } from "react-native";
 import { useState } from "react";
 import { PlayerCard } from "@components/PlayerCard";
+import { ListEmpty } from "@components/ListEmpty";
+import { Button } from "@components/Button";
 
 
 export function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState(['leo', 'vini']);
+  const [players, setPlayers] = useState([]);
   return (
     <Container>
       <Header showBackButton />
@@ -42,7 +44,15 @@ export function Players() {
 
       <FlatList data={players} keyExtractor={item => item} renderItem={({ item }) => (
         <PlayerCard name={item} onRemove={() => { }} />
-      )} />
+      )} ListEmptyComponent={() => <ListEmpty message='Não há pessoas nesse time.' />}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={[
+          { paddingBottom: 100 },
+          players.length === 0 && { flex: 1 }
+        ]}
+      />
+
+      <Button title="Remover Turma" type="SECONDARY" />
 
     </Container>
   )
